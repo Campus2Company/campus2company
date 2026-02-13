e3e787a88739f8298b1651ba48705f730e4331f0
Jakub Sokal <jakubsokal13@gmail.com>
2026-02-13 11:18:24 +0000

    feat: add api-gateway with route config and CORS config (#6)

---
+++ api-gateway/src/test/java/com/campus2company/gateway/ApiGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.campus2company.gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
